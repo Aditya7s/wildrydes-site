428b91f946ec0960911561514c80beaa84e62d25
Aditya Sharma <79127498+Aditya7s@users.noreply.github.com>
2025-05-03 19:36:19 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_edkN06vNi', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1uks90e4evpekefquqkthvjmlr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_4Q7rAZ3nP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4ahkeb9fm3u3kmverqn46j67u7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
